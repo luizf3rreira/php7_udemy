1f91f810b615b75ab36e2534953706f69be382b2
Luiz Ferreira <luiz.f3rreira@gmail.com>
2018-04-26 23:03:35 -0300

    Slim Framework

--- website/index.php
+++ website/index.php
@@ -1,5 +1,22 @@
 <?php 
 
-phpinfo();
+require("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+
+	echo json_encode(array(
+		'date'=>date("Y-m-d H:i:s")
+	));
+
+});
+
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+
+$app->run();
+
 
  ?>
