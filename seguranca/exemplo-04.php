f5ce39d42abff847356cfc60974ad963d9bdf28c
Luiz Ferreira <luiz.f3rreira@gmail.com>
2018-04-27 22:11:02 -0300

    92. Recaptcha

---
+++ seguranca/exemplo-04.php
@@ -0,0 +1,9 @@
+<script src='https://www.google.com/recaptcha/api.js'></script>
+
+<form action="cadastrar.php" method="POS"T>
+	
+	<div class="g-recaptcha" data-sitekey="6LeOBlYUAAAAABcqxHtFU7RrtqpdD_juDxK4VSz-"></div>
+	<input type="email" name="inputEmail">
+	<button type="submit">Enviar</button>
+
+</form>
